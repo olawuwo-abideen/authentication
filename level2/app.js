3c078f30b56948cd52a2944abafcd3a3fa38e96c
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-05-09 04:41:00 +0100

    fix

--- level2/app.js
+++ level2/app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,7 +21,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Itsapassword";
+const secret = process.env.SECRET;
 userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 
